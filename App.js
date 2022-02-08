92f676b3a7d7b852e20246fa28b4b3c578fc9e41
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-02-08 10:33:41 -0600

    2nd commit

--- App.js
+++ App.js
@@ -7,11 +7,12 @@ class App extends React.Component {
         apiKey: 'apikey=4582b002',
         query: '&t=',
         movieTitle: '',
-        searchURL: ''
+        searchURL: '',
+        movie: null , //Use null instead of {} so it wont't show objs/text
     }
 
     handleChange = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         this.setState({
             movieTitle: e.target.value
         })
@@ -25,8 +26,10 @@ class App extends React.Component {
        }, () => {
         //    Fetch returns a promise
         fetch(this.state.searchURL)
-        .then( response => response.json()) // parse to json format
-        .then( movies => console.log(movies)) //Whatever comes back from above
+        .then((response) => response.json()) // parse to json format
+        .then((movie) => {
+            this.setState({movie: movie, movieTitle: ''}); // set movie in the state
+            }) //Whatever comes back from above
         .catch(error => console.error(error)) //handle any errors
        })
     }
@@ -42,11 +45,29 @@ class App extends React.Component {
 
                     <input type='submit'/>
                 </form>
+
+                {/* Conditional for Null above, <Movie/> pass some data */}
+                {this.state.movie && <MovieInfo movie={this.state.movie}/>}
+                {/* or ternary operator*/}
+                {/* {this.state.movie ? <MovieInfo movie={this.state.movie}/> : <h2>Search Again</h2>} */}
             </div>
         )
     }
 } 
 
+//Function Component (When there's no STATE but can use 'props' in both!)
+//Change {} to NUll in state to hide info/title/plot etc
+const MovieInfo = (props) => { 
+    return (
+    <div>
+     <h1>Movie Title:{props.movie.Title}</h1>
+     <h2>Year:{props.movie.Year}</h2>
+     <img src={props.movie.Poster}/>
+     <h3>Genre:{props.movie.Genre}</h3>
+     <p>{props.movie.Plot}</p>
+     </div>
+    )
+}
 
 
 
